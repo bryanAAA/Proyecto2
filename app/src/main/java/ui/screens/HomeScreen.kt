6b462f720b5a5c4bb6b4df40599b8333df633841
bryanAAA <arroyob29@gmail.com>
2025-04-15 11:39:49 -0600

    Cambio en la vista del inicio

--- app/src/main/java/ui/screens/HomeScreen.kt
+++ app/src/main/java/ui/screens/HomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cristianrojas.borrador.ImageCarousel
 import com.cristianrojas.borrador.R
+import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -23,34 +25,42 @@ fun HomeScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp, vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
 
-        ImageCarousel(imageList = images)
-
-        Spacer(modifier = Modifier.height(24.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
         Text(
-            text = "Bienvenido a BugFree Burgers",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(bottom = 8.dp),
-            textAlign = TextAlign.Center
-        )
+                text = "Bienvenido a BugFree Burgers",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(top = 8.dp, bottom = 4.dp),
+                textAlign = TextAlign.Center
+            )
 
-        Text(
-            text = "Disfruta de la mejor experiencia culinaria con un toque de tecnología.",
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(bottom = 16.dp),
-            textAlign = TextAlign.Center
-        )
+            Text(
+                text = "Disfruta de la mejor experiencia culinaria con un toque de tecnología.",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 12.dp),
+                textAlign = TextAlign.Center,
+                lineHeight = 20.sp
+            )
+        }
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ){
+            ImageCarousel(imageList = images)
+        }
 
         Text(
             text = "© 2025 BugFree Burgers - Todos los derechos reservados",
             style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.padding(bottom = 16.dp),
             textAlign = TextAlign.Center
         )
     }
